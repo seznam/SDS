be20c4b7eec989d30c7267c4155a20073ff86481
ethanius <jakub.kraft@firma.seznam.cz>
2018-11-25 18:25:25 +0100

    zjednoduseni + anglicka dokumentace Spinner

--- react/src/Spinner/index.js
+++ react/src/Spinner/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import { classNames } from "@sammas/helpers";
 
 /**
- * Komponenta Icon představující jednu z vestavěných SVG ikon dodávaných v rámci design systému
- * @param {object} props Objekt s atributy komponenty
- * @param {string} [props.className] Mezerami oddělený seznam CSS tříd, které se přidají k těm, které Icon interně využívá
+ * Icon component represents one of the built-in icons packed with Sammas
+ * @param {object} props An object with props
+ * @param {string} [props.className] Space separated list of CSS classes to be added to those that Spinner uses internaly
  */
-const Spinner = ({ className = "", ...props }) => {
-	return (
-		<span className={classNames(["sammas-spinner", className])} {...props}></span>
-	);
-};
+const Spinner = ({ className = "", ...props }) => <span className={classNames(["sammas-spinner", className])} {...props}></span>;
 
 export default Spinner;
